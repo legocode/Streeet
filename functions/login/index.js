579c75d1d8a271baa55eb41d55245cfea8306db0
Leon Li <lilei.leon@gmail.com>
2016-06-30 15:04:42 +0800

    template mapping

--- functions/login/index.js
+++ functions/login/index.js
@@ -3,10 +3,10 @@ import λ from 'apex.js';
 import {login} from '../../lib/user_model';
 
 export default λ((e, ctx) => {
-  if(!e.username || !e.password) return {"errorMessage": "username and password cannot be empty"}
+  if(!e['body-json'].username || !e['body-json'].password) return {"errorMessage": "username and password cannot be empty"}
   const params = {
-    username: e.username,
-    password: e.password
+    username: e['body-json'].username,
+    password: e['body-json'].password
   };
   return login(params);
 });
