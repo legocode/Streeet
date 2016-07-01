a93dab11ac15613ecd6710f0bc494072d8eafda5
jimliu123 <jimliu@baobaoj.com>
2016-07-01 12:33:11 +0800

    post api

--- functions/posts/index.js
+++ functions/posts/index.js
@@ -1,42 +1,28 @@
 import 'babel-polyfill';
 import λ from 'apex.js';
 import {decodeToken} from '../../lib/authorizer';
+import {getAll, getById, addPost} from '../../lib/post_model';
 
 export default λ((e, ctx) => {
   const postId = e.params.path.id;
   const httpMethod = e.context['http-method'];
-  if (!postId)
-    return index(e);
+
+  if (!postId) {
+    if (httpMethod === "GET"){
+      return getAll();
+    }
+    if (httpMethod === "POST") {
+      const params = {
+        token: e.params.header.Authorization,
+        imageURL: e['body-json'].imageURL,
+        desc: e['body-json'].desc
+      };
+      return addPost(params);
+    }
+
+  }
+
   if (postId) {
-    if (httpMethod === "GET") 
-      return getById(e, postId);
-    if (httpMethod === "POST")
-      return postById(e, postId);
+      return getById(postId);
   }
 });
-
-const index = async e => {
-  try {
-    const claims = await decodeToken(e.params.header.Authorization);
-    const userInfo = claims.body.userInfo;
-    return {
-      e,
-      claims,
-      userInfo
-    };
-  } catch (ex) {
-    throw new Error({"errorMessage": ex})
-  }
-}
-
-const getById = async (e, id) => {
-  const claims = await decodeToken(e.params.header.Authorization);
-  const userInfo = claims.body.userInfo;
-  return {post_id: id, method: e.context['http-method'], userInfo};
-}
-
-const postById = async (e, id) => {
-  const claims = await decodeToken(e.params.header.Authorization);
-  const userInfo = claims.body.userInfo;
-  return {post_id: id, method: e.context['http-method'], userInfo};
-}
