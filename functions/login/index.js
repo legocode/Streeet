02b77bf8e356784608bb2e81432ee753a2d11fa8
Leon Li <lilei.leon@gmail.com>
2017-03-17 14:50:40 +0800

    correct the !

--- functions/login/index.js
+++ functions/login/index.js
@@ -3,7 +3,7 @@ import λ from 'apex.js';
 import {login} from '../../lib/user_model';
 
 export default λ((e, ctx) => {
-  if(!e['body-json'].username || !e['body-json'].password) return {"errorMessage": "username and password cannot be empty..."}
+  if(!e['body-json'].username || !e['body-json'].password) return {"errorMessage": "username and password cannot be empty!"}
   const params = {
     username: e['body-json'].username,
     password: e['body-json'].password
